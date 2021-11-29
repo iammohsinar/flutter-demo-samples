e1bbf5fe65dd6694cb434ec20bac912f5f54f596
Mohsin AR <mohsinazeemrind@gmail.com>
2021-11-29 18:28:16 +0500

    working on save book

--- desktop_library_shop/lib/ui/views/book_view.dart
+++ desktop_library_shop/lib/ui/views/book_view.dart
@@ -1,5 +1,6 @@
 import 'package:desktop_library_shop/ui/util/app_color.dart';
 import 'package:desktop_library_shop/ui/util/ui_util.dart';
+import 'package:desktop_library_shop/ui/widgets/app_button.dart';
 import 'package:desktop_library_shop/ui/widgets/app_panel.dart';
 import 'package:desktop_library_shop/ui/widgets/app_tab.dart';
 import 'package:desktop_library_shop/ui/widgets/app_text.dart';
@@ -17,10 +18,12 @@ class BookView extends StatefulWidget {
 
 class _BookViewState extends State<BookView> {
   late FocusNode _bookIdFocus;
-
   late FocusNode _barCodeIdFocus;
   late FocusNode _referenceNoFocus;
   late FocusNode _bookTitleFocus;
+  late FocusNode _addBtnFocus;
+  late FocusNode _updateBtnFocus;
+  late FocusNode _cancelBtnFocus;
 
   @override
   void initState() {
@@ -28,6 +31,9 @@ class _BookViewState extends State<BookView> {
     _barCodeIdFocus = FocusNode();
     _referenceNoFocus = FocusNode();
     _bookTitleFocus = FocusNode();
+    _addBtnFocus = FocusNode();
+    _updateBtnFocus = FocusNode();
+    _cancelBtnFocus = FocusNode();
 
     super.initState();
   }
@@ -146,7 +152,7 @@ class _BookViewState extends State<BookView> {
                             //   ],
                             // )),
                             Expanded(
-                                //flex: 2,
+                                // flex: 2,
                                 child: Row(
                               mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
@@ -154,21 +160,18 @@ class _BookViewState extends State<BookView> {
                                 AppTextLabel('Category'),
                                 UIUtil.hXSmallSpace(),
                                 Flexible(
-                                  flex: 2,
                                   child: AppDropDown(),
                                 ),
                                 UIUtil.hSmallSpace(),
                                 AppTextLabel('Author'),
                                 UIUtil.hXSmallSpace(),
                                 Flexible(
-                                  flex: 2,
                                   child: AppDropDown(),
                                 ),
                                 UIUtil.hSmallSpace(),
                                 AppTextLabel('Publisher'),
                                 UIUtil.hXSmallSpace(),
                                 Flexible(
-                                  flex: 2,
                                   child: AppDropDown(),
                                 ),
                                 UIUtil.hSmallSpace(),
@@ -184,121 +187,140 @@ class _BookViewState extends State<BookView> {
                             //   ],
                             // )),
                             Expanded(
-                                //flex: 2,
-                                child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    AppTextLabel('Publish year'),
-                                    UIUtil.hXSmallSpace(),
-                                    Flexible(
+                              //flex: 2,
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  AppTextLabel('Publish year'),
+                                  UIUtil.hXSmallSpace(),
+                                  Flexible(
+                                    child: AppTextFormField(
+                                        maxLength: 20,
+                                        //icon: Icons.library_books,
+                                        //width: 190,
+                                        height: 45,
+                                        current: _referenceNoFocus,
+                                        next: _bookTitleFocus,
+                                        validationMsg: 'Please type reference number'),
+                                  ),
+                                  UIUtil.hSmallSpace(),
+                                  AppTextLabel('Total Copies ?'),
+                                  UIUtil.hXSmallSpace(),
+                                  Flexible(
+                                      fit: FlexFit.tight,
                                       child: AppTextFormField(
-                                          maxLength: 20,
+                                          maxLength: 100,
                                           //icon: Icons.library_books,
-                                          //width: 190,
+                                          // width: 190,
                                           height: 45,
-                                          current: _referenceNoFocus,
-                                          next: _bookTitleFocus,
-                                          validationMsg: 'Please type reference number'),
-                                    ),
-                                    UIUtil.hSmallSpace(),
-                                    AppTextLabel('Total Copies ?'),
-                                    UIUtil.hXSmallSpace(),
-                                    Flexible(
-                                        fit: FlexFit.tight,
-                                        child: AppTextFormField(
-                                            maxLength: 100,
-                                            //icon: Icons.library_books,
-                                            // width: 190,
-                                            height: 45,
-                                            current: _bookTitleFocus,
-                                            next: _bookIdFocus,
-                                            validationMsg: 'Please type reference number')),
+                                          current: _bookTitleFocus,
+                                          next: _bookIdFocus,
+                                          validationMsg: 'Please type reference number')),
 
-                                    UIUtil.hSmallSpace(),
-                                    AppTextLabel('Language'),
-                                    UIUtil.hXSmallSpace(),
-                                    Flexible(
-                                        flex: 2,
-                                        child: AppTextFormField(
-                                            maxLength: 100,
-                                            //icon: Icons.library_books,
-                                            // width: 190,
-                                            height: 45,
-                                            current: _bookTitleFocus,
-                                            next: _bookIdFocus,
-                                            validationMsg: 'Please type reference number')),
-                                    UIUtil.hSmallSpace(),
-                                    AppTextLabel('Description'),
-                                    UIUtil.hXSmallSpace(),
-                                    Flexible(
-                                        flex: 4,
-                                        child:
+                                  UIUtil.hSmallSpace(),
+                                  AppTextLabel('Language'),
+                                  UIUtil.hXSmallSpace(),
+                                  Flexible(
+                                      flex: 2,
+                                      child: AppTextFormField(
+                                          maxLength: 100,
+                                          //icon: Icons.library_books,
+                                          // width: 190,
+                                          height: 45,
+                                          current: _bookTitleFocus,
+                                          next: _bookIdFocus,
+                                          validationMsg: 'Please type reference number')),
+                                  UIUtil.hSmallSpace(),
+                                  AppTextLabel('Description'),
+                                  UIUtil.hXSmallSpace(),
+                                  Flexible(
+                                      flex: 4,
+                                      child:
 
-                                            // TextFormField(
-                                            //   minLines: 1,
-                                            //   decoration: InputDecoration(
-                                            //       isDense: true,
-                                            //       counterText: ' ',
-                                            //       //  prefixIcon: (icon != null) ? Icon(icon) : null, //icon ?? Icon(icon),
-                                            //       // enabled: isEnable,
-                                            //       contentPadding: const EdgeInsets.only(
-                                            //           top: 15,
-                                            //           left: 10), //const EdgeInsets.all(10.0),,
-                                            //       // fillColor: (isEnable) ? fieldActiveColor : fieldFocusColor,
-                                            //       filled: true,
-                                            //       border: const OutlineInputBorder(
-                                            //           borderSide: BorderSide(
-                                            //               color: Colors.black, width: 0.8)),
-                                            //       focusedBorder: const OutlineInputBorder(
-                                            //           borderSide: BorderSide(
-                                            //               color: borderColor, width: 0.8)),
-                                            //       focusedErrorBorder: const OutlineInputBorder(
-                                            //           borderSide: BorderSide(
-                                            //               color: errorBorderColor, width: 0.8)),
-                                            //       errorBorder: const OutlineInputBorder(
-                                            //           borderSide: BorderSide(
-                                            //               color: errorBorderColor, width: 0.8)),
-                                            //       enabledBorder: const OutlineInputBorder(
-                                            //           borderSide: BorderSide(
-                                            //               color: borderColor, width: 0.8))),
-                                            //   maxLines: 4,
-                                            // )
-                                            AppTextFormField(
-                                                maxLines: 5,
-                                                //maxLength: 100,
-                                                //icon: Icons.library_books,
-                                                // width: 190,
-                                                height: 100,
-                                                current: _bookTitleFocus,
-                                                next: _bookIdFocus,
-                                                validationMsg:
-                                                    'Please type reference number')),
-                                    // UIUtil.hSmallSpace(),
-                                    // AppTextLabel('Book Title'),
-                                    // UIUtil.hXSmallSpace(),
-                                    // Flexible(
-                                    //     flex: 10,
-                                    //     fit: FlexFit.tight,
-                                    //     child: AppTextFormField(
-                                    //         maxLength: 100,
-                                    //         //icon: Icons.library_books,
-                                    //         // width: 190,
-                                    //         height: 45,
-                                    //         current: _bookTitleFocus,
-                                    //         next: _bookIdFocus,
-                                    //         validationMsg: 'Please type reference number'))
-                                  ],
-                                ),
-                                Row(
-                                  children: [Expanded(child: Container())],
-                                )
-                              ],
-                            )),
+                                          // TextFormField(
+                                          //   minLines: 1,
+                                          //   decoration: InputDecoration(
+                                          //       isDense: true,
+                                          //       counterText: ' ',
+                                          //       //  prefixIcon: (icon != null) ? Icon(icon) : null, //icon ?? Icon(icon),
+                                          //       // enabled: isEnable,
+                                          //       contentPadding: const EdgeInsets.only(
+                                          //           top: 15,
+                                          //           left: 10), //const EdgeInsets.all(10.0),,
+                                          //       // fillColor: (isEnable) ? fieldActiveColor : fieldFocusColor,
+                                          //       filled: true,
+                                          //       border: const OutlineInputBorder(
+                                          //           borderSide: BorderSide(
+                                          //               color: Colors.black, width: 0.8)),
+                                          //       focusedBorder: const OutlineInputBorder(
+                                          //           borderSide: BorderSide(
+                                          //               color: borderColor, width: 0.8)),
+                                          //       focusedErrorBorder: const OutlineInputBorder(
+                                          //           borderSide: BorderSide(
+                                          //               color: errorBorderColor, width: 0.8)),
+                                          //       errorBorder: const OutlineInputBorder(
+                                          //           borderSide: BorderSide(
+                                          //               color: errorBorderColor, width: 0.8)),
+                                          //       enabledBorder: const OutlineInputBorder(
+                                          //           borderSide: BorderSide(
+                                          //               color: borderColor, width: 0.8))),
+                                          //   maxLines: 4,
+                                          // )
+                                          AppTextFormField(
+                                              maxLines: 5,
+                                              //maxLength: 100,
+                                              //icon: Icons.library_books,
+                                              // width: 190,
+                                              height: 100,
+                                              current: _bookTitleFocus,
+                                              next: _bookIdFocus,
+                                              validationMsg: 'Please type reference number')),
+                                  // UIUtil.hSmallSpace(),
+                                  // AppTextLabel('Book Title'),
+                                  // UIUtil.hXSmallSpace(),
+                                  // Flexible(
+                                  //     flex: 10,
+                                  //     fit: FlexFit.tight,
+                                  //     child: AppTextFormField(
+                                  //         maxLength: 100,
+                                  //         //icon: Icons.library_books,
+                                  //         // width: 190,
+                                  //         height: 45,
+                                  //         current: _bookTitleFocus,
+                                  //         next: _bookIdFocus,
+                                  //         validationMsg: 'Please type reference number'))
+                                ],
+                              ),
+                            ),
                           ],
                         ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Row(
+                                children: [
+// add/update/cancel buttons
+                                  AppElevatedBtn(
+                                      imgUrl: 'assets/images/icons/save.png',
+                                      focusNode: _addBtnFocus,
+                                      isEnable: true,
+                                      onPressedFn: () {
+                                        _addBtnFocus.unfocus();
+                                        FocusScope.of(context).requestFocus(_barCodeIdFocus);
+                                      },
+                                      text: 'Save')
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  // print
+                                ],
+                              ),
+                            )
+                          ],
+                        )
                         // Row(
                         //     mainAxisAlignment: MainAxisAlignment.start,
                         //     crossAxisAlignment: CrossAxisAlignment.start,
